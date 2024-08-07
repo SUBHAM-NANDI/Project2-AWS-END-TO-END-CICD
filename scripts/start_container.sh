fea4aeb3acc42960a9cb7f3c70ca6a04d82a34b0
Subham Nandi <subhamnandiagt@gmail.com>
2024-08-07 19:30:54 +0530

    add the fies to code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull subhamnandi/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+docker run -p -d 5000:5000 subhamnandi/simple-python-flask-app:latest
